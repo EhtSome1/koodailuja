460757dcfee1ce605f8f38d81976e82030418ee9
Alpo Uusitalo <arppauu@gmail.com>
2023-06-07 14:51:16 +0300

    python laksin kielivaihtoehto

--- pythonLaskin.py
+++ pythonLaskin.py
@@ -2,31 +2,71 @@ import os
 vast = 0
 x = 0
 vastnum = 3
+kieli = 0
+vastkieli = 0
 
-print("+----------------------------------------------------+")
-print("|    Hei! Haluatko että lasken laskun puolestasi?    |")
-print("|    (Kirojoita numero)                              |")
-print("|    1 - Kyllä                                       |")
-print("|    2 - Ei                                          |")
-print("+----------------------------------------------------+")
+
+print("+-------------------------+")
+print("|    Language / Kieli     |")
+print("|                         |")
+print("|    1 - Suomi            |")
+print("|    2 - English          |")
+print("+-------------------------+")
 
 while x == 0:
-    vast = input()
-    print("vast:", vast)
-    try:
-        vastnum = int(vast)
+    kieli = input()
+    #print("vast:", vast)
+    try:    # check if inputted symbol is a number, if not ask again
+        vastkieli = int(kieli)
         x = 1
-        print("x:", x, "vastnum:", vastnum)
+        #print("x:", x, "vastnum:", vastnum)
 
-        if vastnum != 1 and vastnum != 2:
+        if vastkieli != 1 and vastkieli != 2:
             raise
 
     except:
         x = 0
-        print("x", x)
-        print("Anna vain kysytty numero!")
+        #print("x", x)
+        print("Only giva a asked number! / Anna vain kysytty numero!")
+os.system("cls")
 
-if vastnum == 1:
-    #os.system('cls')
-    print("onnistui")
+x = 0
+if vastkieli == 1:
+    print("+----------------------------------------------------+")
+    print("|    Hei! Haluatko että lasken laskun puolestasi?    |")
+    print("|                                                    |")
+    print("|    1 - Kyllä                                       |")
+    print("|    2 - Ei                                          |")
+    print("+----------------------------------------------------+")
 
+    while x == 0:
+        vast = input()
+        #print("vast:", vast)
+        try:    # check if inputted symbol is a number, if not ask again
+            vastnum = int(vast)
+            x = 1
+            #print("x:", x, "vastnum:", vastnum)
+
+            if vastnum != 1 and vastnum != 2:
+                raise
+
+        except:
+            x = 0
+            #print("x", x)
+            print("Anna vain kysytty numero!")
+
+    if vastnum == 1:
+        #os.system('cls')
+        #print("onnistui")
+
+        print("+--------------------------+")
+        print("|    Minkälainen lasku?    |")
+        print("|                          |")
+        print("|    1 - Pluslasku         |")
+        print("|    2 - Miinuslasku       |")
+        print("|    2 - Kertolasku        |")
+        print("|    2 - Kertolasku        |")
+        print("|    2 - Kertolasku        |")
+        print("|    2 - Kertolasku        |")
+        print("|    2 - Kertolasku        |")
+        print("+--------------------------+")
